3aa75b9f0a3d47192152b7b77e3af5c0651ab667
KLacerda08 <kaldmr7880@gmail.com>
2021-01-27 20:11:53 -0500

    fixed y-axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,4 +1,3 @@
-// var svgWidth = 960;
 var svgWidth = 800;
 var svgHeight = 500;
 
@@ -6,15 +5,13 @@ var margin = {
   top: 20,
   right: 60,
   bottom: 60,
-  // left: 100
-  left: 80
+  left: 55
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-// var svg = d3.select(".chart")
 var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
@@ -31,13 +28,7 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
       record.income = +record.income;
       record.smokes = +record.smokes;
       record.abbr = record.abbr;
-      // console.log(record.abbr);
     });
-
-// var income = d3.csv("assets/data/data.csv").then(function(healthData) {
-//   healthData.forEach(function(record) {
-//   record.abbr})};
-//     console.log(income)
 
   // define variables for chart limits based on data needs. 
   var xMin = d3.min(healthData, record => record.income - 2000)
@@ -48,17 +39,6 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
   console.log(xMax);
   console.log(yMin);
 
-
-    // Create scale functions
-    // var xLinearScale = d3.scaleLinear()
-    //   .domain([20, d3.max(healthData, record => record.income)])
-    //   .range([0, width]);
-
-    // var yLinearScale = d3.scaleLinear()
-    //   .domain([0, d3.max(healthData, record => record.smokes)])
-    //   .range([height, 0]);
-
-
         // Create scale functions for axis length; try zero to start
     // xAxis - income
     var xLinearScale = d3.scaleLinear()
@@ -68,7 +48,8 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     // yAxis - smokes
     var yLinearScale = d3.scaleLinear()
     .domain([yMin, yMax])
-    .range([0, height]); 
+    .range([height, 0])
+    ; 
 
     // Create axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
@@ -94,7 +75,6 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     .attr("opacity", ".5")
     .attr("stroke-width", "2")
     .attr("stroke", "purple");
-    // .text(record => record.abbr)
     ;
 
     chartGroup.select("g")
@@ -105,26 +85,23 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     .text(record => record.abbr)
     .attr("x", record => xLinearScale(record.income))
     .attr("y", record => yLinearScale(record.smokes))
-    // .attr("dy",-395)
-    .attr("dy",-415)
+    // shift state abbreviations to fit into circles. pixel number appears to 
+    //  correlate to the bottom of uppermost circle (i.e. at dy = 0, the bottom of 
+    //  lable "UT" is in line with the top of the x-axis title)
+    .attr("dy", -415)
     .attr("text-anchor", "middle")
     .attr("font-size", "12px")
     .attr("fill", "black")
     .attr("font-weight", "bold");
 
     // Initialize tool tip
-    // .d3-tip
-    // var toolTip = d3.tip()
     var toolTip = d3.tip()
-    .attr("class", ".d3-tip")
-      // .offset([80, -60])
-      // .attr ("text", "center")
+      .attr("class", ".d3-tip")
       .style("font", "14px arial")
       .style("font", "bold")
       .style("border", "2px solid purple")
       .style("border-radius", "5px")
       .style("text-align", "center")
-      // .style("background-color", "#6F257F")
       .style("background-color", "lavender")
       .html(function(record) {
         return (`${record.state}<br>% Who Smoke: ${record.smokes}<br>Income (USD): ${record.income}`);
@@ -147,8 +124,7 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      // .attr("y", 0 - margin.left + 40)
-      .attr("y", 0 - margin.left + 20)
+      .attr("y", 0 - margin.left + 5)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "aText")
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# D3-Challenge
+# Data Journalism - Smoking and Income Trends  
+
+![Title](images/smoking_money.jpg)
+
+## Table of contents
+* [Assignment](#assignment)
+* [Data Sources](#data_sources)
+* [Dashboard Design](#design)
+* [Evaluation](#eval)
+* [References](#ref)
+
+## Assignment
+Use JavaScript with html, D3, scalable vector graphics (svg) to generate an interactive chart of health risks based on data from the US Census Bureau. 
+
+
+## Data Sources
+Style sheets and starter code were provided, as was a csv containing US Census Bureau data.  Original files can be found at the links below: 
+
+* Belly button bacteria data: https://github.com/KLacerda08/plotly-challenge/tree/main/Resources/samples.json
+* original data from: http://robdunnlab.com/projects/belly-button-biodiversity/ 
+* html: https://github.com/KLacerda08/plotly-challenge/tree/main/Resources/index.html
+* app.js file: https://github.com/KLacerda08/plotly-challenge/tree/main/Resources/static/js/app.js
+
+
+## Dashboard Design
+The index.html file contained the framework to store the dashboard, including a dropdown menu (to be populated), and html IDs where the bar plotand bubble plot could be tagged. Code was designed to first
+populate the dropdown menu, and then to build the plots. During development, a dummy patient index was utilized to test the code in the console log.  After completion, the dummy index was removed. Functions were 
+developed to build the bar and bubble plots, and the demographic data of the patient, utizing the following fields from the dataset:
+- sample_values
+- otu_ids
+- otu_labels 
+
+To operate the dashboard, select the Test Subject ID from the dropdown menu.  The bubble plot shows all the microbial groups (operational taxonomic units or OTUs) for that patient; the markers are sized in relation to the count of the OTU present. The bar plot shows the top ten OTUs in the selected patient. In some cases, the patient did not have ten OTUs present, in which case fewer are shown. An example of the dashboard output is provide below.  
+(patient 1601).  
+![Title](images/plots.png)
+
+## Evaluation
+The data shows that certain OTUs, such as 944, 1795, and 2419 are common across many individuals in substantial concentrations.  Additional analysis could be performed with statistics to show which OTU(s) are most common across the population. 
+
+## References
+Readme image source: 
+https://www.unh.edu/healthyunh/blog/tobacco/2017/11/tobacco-and-your-wallet
+https://depositphotos.com/140091338/stock-photo-data-science-newspaper-article-text.html
